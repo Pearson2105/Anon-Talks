852139578a08c872263e3eaa01cdb52824296b5b
Pearson2105 <157844357+Pearson2105@users.noreply.github.com>
2025-12-09 19:08:01 +0000

    Update extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -5,5 +5,4 @@ from flask_limiter.util import get_remote_address
 
 db = SQLAlchemy()
 cors = CORS()
-# Limiter is initialized here with key_func
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
