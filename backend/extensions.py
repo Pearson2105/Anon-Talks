01894f23b88dd0b9ad9748ef3f3104c0399104da
Pearson2105 <157844357+Pearson2105@users.noreply.github.com>
2025-12-09 18:49:15 +0000

    Update extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -5,4 +5,4 @@ from flask_limiter.util import get_remote_address
 
 db = SQLAlchemy()
 cors = CORS()
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(key_func=get_remote_address)  # key_func set here
