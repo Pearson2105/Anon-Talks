2885ce0f4e1b80a66f176ffdc1d4562253e322b4
Pearson2105 <157844357+Pearson2105@users.noreply.github.com>
2025-12-09 18:51:07 +0000

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,10 @@
 import os
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or f"sqlite:///{os.path.join(BASE_DIR, 'data.db')}"
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
-  JSON_SORT_KEYS = False
-  RATELIMIT_DEAFULT = "200 per day;50 per hour"
-  RATELIMIT_STORAGE_URI = "memory://"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or f"sqlite:///{os.path.join(BASE_DIR, 'data.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_SORT_KEYS = False
+    RATELIMIT_DEFAULT = "200 per day;50 per hour"   # Fixed typo
+    RATELIMIT_STORAGE_URI = "memory://"            # Fixed extra quote
