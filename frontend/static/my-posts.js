9726c3df3b4e2ee1aa82aea6cdb154681840c998
Pearson2105 <157844357+Pearson2105@users.noreply.github.com>
2025-12-10 13:30:00 +0000

    Update my-posts.js

--- frontend/static/my-posts.js
+++ frontend/static/my-posts.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
 
     document.getElementById("backBtn").addEventListener("click", () => {
-        window.location.href = "../index.html";
+        window.location.href = "../select.html";
     });
 });
 
