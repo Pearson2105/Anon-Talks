d9fd6e900235f603970b8ec15ea8930e0706193b
Pearson2105 <157844357+Pearson2105@users.noreply.github.com>
2025-12-11 22:23:17 +0000

    Update popups.js

--- static/popups.js
+++ static/popups.js
@@ -1,8 +1,9 @@
-// static/popups.js
-export function showPopup(popup) {
-    popup?.classList.remove("hidden");
+export function showPopup(el) {
+    if (!el) return;
+    el.classList.remove("hidden");
 }
 
-export function hidePopup(popup) {
-    popup?.classList.add("hidden");
+export function hidePopup(el) {
+    if (!el) return;
+    el.classList.add("hidden");
 }
