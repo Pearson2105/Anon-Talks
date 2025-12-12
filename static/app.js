ecfa5b2aa9cc0d2d20e2e0401ea16c8654172db4
Pearson2105 <157844357+Pearson2105@users.noreply.github.com>
2025-12-12 03:14:25 +0000

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 import { initAuth } from "./auth.js";
 import { initPosts } from "./posts.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", ()=>{
     initAuth();
-    initPosts();  // runs only on select.html because it checks elements
+    initPosts();
 });
