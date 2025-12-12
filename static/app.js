d08d602d16a41a606771a622525aced40313dc60
Pearson2105 <157844357+Pearson2105@users.noreply.github.com>
2025-12-12 01:37:12 +0000

    Update app.js

--- static/app.js
+++ static/app.js
@@ -2,6 +2,7 @@ import { initAuth } from "./auth.js";
 import { initPosts } from "./posts.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("app DOMContentLoaded");
     initAuth();
     initPosts();
 });
