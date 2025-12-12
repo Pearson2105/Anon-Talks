127987003fbc53d430bc0270b5ca94d258a5eba0
Pearson2105 <157844357+Pearson2105@users.noreply.github.com>
2025-12-12 10:44:09 +0000

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,6 +1,7 @@
 import { initAuth } from "./auth.js";
 import { initPosts } from "./posts.js";
 
+// Initialize login system and posts system when page loads
 document.addEventListener("DOMContentLoaded", ()=>{
     initAuth();
     initPosts();
